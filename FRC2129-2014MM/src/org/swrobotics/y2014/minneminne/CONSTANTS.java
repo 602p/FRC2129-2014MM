c63f3ae1052b1beaeada92a2f5d047d5507d9dac
Louis Goessling <louis@goessling.com>
2014-11-21 18:06:28 -0600

    Actual code commit

--- FRC2129-2014MM/src/org/swrobotics/y2014/minneminne/CONSTANTS.java
+++ FRC2129-2014MM/src/org/swrobotics/y2014/minneminne/CONSTANTS.java
@@ -9,15 +9,32 @@ package org.swrobotics.y2014.minneminne;
  * @author Louis
  */
 public final class CONSTANTS {
-    public static int     LEFT_DRIVE_PWM    = 1;
-    public static int     RIGHT_DRIVE_PWM   = 2;
-    public static int     ROLLER_PWM        = 3;
-    public static int     HINGE_PWM         = 4;
+    public static int     LEFT_DRIVE_PWM       = 1;
+    public static int     RIGHT_DRIVE_PWM      = 2;
+    public static int     ROLLER_PWM           = 3;
+    public static int     HINGE_PWM            = 4;
     
-    public static int     LEFT_JOY_ID       = 1;
-    public static int     RIGHT_JOY_ID      = 1;
+    public static int     HINGE_TOP_LIMIT      = 2;
     
-    public static int     DSA_DRIVESCALE_ID = 4;
     
-    public static boolean ENABLE_DRIVE      = true;
+    public static int     LEFT_JOY_ID          = 1;
+    public static int     RIGHT_JOY_ID         = 2;
+    
+    public static int     DSA_DRIVESCALE_ID    = 4;
+    public static int     DSA_ROLLERSPEED_ID   = 1;
+    public static int     DSA_HINGESPEED_ID    = 2;
+    
+    public static int     DSB_LIMITOVERRIDE_ID = 2;
+    
+    public static boolean ENABLE_DRIVE         = true;
+    public static boolean ENABLE_ROLLER        = true;
+    public static boolean ENABLE_HINGE         = true;
+    public static boolean ENABLE_LIMIT_SWITCH  = false;
+    
+    public static int     ROLLER_UP_BTN        = 3;
+    public static int     ROLLER_DOWN_BTN      = 1;
+    public static int     HINGE_UP_BTN         = 3;
+    public static int     HINGE_DOWN_BTN       = 1;
+    
+    
 }
--- FRC2129-2014MM/src/org/swrobotics/y2014/minneminne/Team2129MMRobot.java
+++ FRC2129-2014MM/src/org/swrobotics/y2014/minneminne/Team2129MMRobot.java
@@ -1,7 +1,9 @@
 
 package org.swrobotics.y2014.minneminne;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
@@ -9,20 +11,39 @@ import edu.wpi.first.wpilibj.SimpleRobot;
 
 public class Team2129MMRobot extends SimpleRobot {
     public RobotDrive robotdrive = new RobotDrive(CONSTANTS.LEFT_DRIVE_PWM, CONSTANTS.RIGHT_DRIVE_PWM);
-    public Joystick leftjoy = new Joystick(CONSTANTS.LEFT_JOY_ID);
-    public Joystick rightjoy = new Joystick(CONSTANTS.RIGHT_JOY_ID);
+    public Joystick leftjoystick = new Joystick(CONSTANTS.LEFT_JOY_ID);
+    public Joystick rightjoystick = new Joystick(CONSTANTS.RIGHT_JOY_ID);
     public DriverStation driverstation = DriverStation.getInstance();
-    
-    public void autonomous(){
-        
-    }
+    public Jaguar rollerJaguar = new Jaguar(CONSTANTS.ROLLER_PWM);
+    public Jaguar hingeJaguar = new Jaguar(CONSTANTS.HINGE_PWM);
+    public DigitalInput hingeTopLimit = new DigitalInput(CONSTANTS.HINGE_TOP_LIMIT);
     
     public void operatorControl(){
         while (isOperatorControl()&&isEnabled()){
             if (CONSTANTS.ENABLE_DRIVE) {
-                this.robotdrive.tankDrive(this.leftjoy.getX()*this.driverstation.getAnalogIn(CONSTANTS.DSA_DRIVESCALE_ID),
-                    this.leftjoy.getY()*this.driverstation.getAnalogIn(CONSTANTS.DSA_DRIVESCALE_ID));
+                this.robotdrive.tankDrive(this.leftjoystick.getY()*this.driverstation.getAnalogIn(CONSTANTS.DSA_DRIVESCALE_ID),
+                    this.rightjoystick.getY()*this.driverstation.getAnalogIn(CONSTANTS.DSA_DRIVESCALE_ID));
             }
+            if (CONSTANTS.ENABLE_ROLLER){
+                if (this.rightjoystick.getRawButton(CONSTANTS.ROLLER_UP_BTN)){
+                    this.rollerJaguar.set(this.driverstation.getAnalogIn(CONSTANTS.DSA_ROLLERSPEED_ID));
+                }else if (this.rightjoystick.getRawButton(CONSTANTS.ROLLER_DOWN_BTN)){
+                    this.rollerJaguar.set(-this.driverstation.getAnalogIn(CONSTANTS.DSA_ROLLERSPEED_ID));
+                }else{
+                    this.rollerJaguar.set(0);
+                }
+            }
+            if (CONSTANTS.ENABLE_HINGE){
+                if (this.leftjoystick.getRawButton(CONSTANTS.HINGE_UP_BTN) & ((this.hingeTopLimit.get() || this.driverstation.getDigitalIn(CONSTANTS.DSB_LIMITOVERRIDE_ID)) || !CONSTANTS.ENABLE_LIMIT_SWITCH)){
+                    this.hingeJaguar.set(this.driverstation.getAnalogIn(CONSTANTS.DSA_HINGESPEED_ID));
+                }else if (this.leftjoystick.getRawButton(CONSTANTS.HINGE_DOWN_BTN)){
+                    this.hingeJaguar.set(-this.driverstation.getAnalogIn(CONSTANTS.DSA_HINGESPEED_ID));
+                }else{
+                    this.hingeJaguar.set(0);
+                }
+            }
+            
+            this.driverstation.setDigitalOut(1, this.hingeTopLimit.get());
         }
     }
 }
